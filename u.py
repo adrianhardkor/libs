d8416fab3d818975f8c1df7e144b2d7b4ec8d93f
adrianhardkor <adrian.krygowski@wowinc.com>
2021-07-23 13:33:32 -0400

    cleanup

--- u.py
+++ u.py
@@ -10,11 +10,8 @@ import time
 import requests
 import concurrent.futures
 import yaml
-print(yaml.dump(json.loads(wc.read_file('/opt/FlaskAPI/validateDCIM/_logic/tmp.json')))); exit()
 
-wc.jd(json.loads(wc.read_file('/opt/FlaskAPI/validateDCIM/_logic/tmp.json'))); exit()
-
-wc.jd(wc.PARA_CMD_LIST(ip='10.88.241.7', commands=['show interface all status', 'show port async all status'], driver='mrvTS', username='arcadmin', password='arcaccess', become='arcenable', quiet=False,ping=False,windowing=True, settings_prompt="([a-zA-Z0-9\-\_]+[0-9\:\ ]+[>])", buffering=['no pause'], exit=['exit']))
+wc.jd(wc.PARA_CMD_LIST(ip='10.88.241.7', commands=['show interface all status', 'show port async all status'], driver='mrvTS2', username='arcadmin', password='arcaccess', become='arcenable', quiet=False,ping=False,windowing=True, settings_prompt="([a-zA-Z0-9\-\_]+[\:]+[0-9\ ]+[>])", buffering=['en', 'no pause'], exit=['exit']))
 
 
 
